b984ba10734a92415ee6f3aec8d2b0abe957182f
oshtaier <alexshtayer@yahoo.com>
2018-02-02 18:07:22 +0200

    adding arch_os test

--- tests/foreman/ui_airgun/test_architecture.py
+++ tests/foreman/ui_airgun/test_architecture.py
@@ -1,8 +1,18 @@
 from airgun.decorators import parametrize
-from robottelo.datafactory import valid_data_list
+from robottelo.datafactory import gen_string, valid_data_list
 
 
 @parametrize('name', valid_data_list())
 def test_positive_create(session, name):
     with session:
         session.architecture.create_architecture({'name': name})
+
+
+def test_positive_create_with_os(session):
+    name = gen_string('alpha')
+    with session:
+        session.architecture.create_architecture({
+            'name': name,
+            'os_dict_values': {
+                'operation': 'Add', 'values': ['RedHat 59.9', 'RedHat 7.3']}
+        })
